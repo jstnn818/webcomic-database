39ee1a77038e39b14fa424fcfbf1470840f7877e
jstnn818 <jstnn818@gmail.com>
2023-11-19 21:33:20 -0800

    title rename

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -14,7 +14,7 @@ const Navbar = () => {
         <header>
             <div className="container">
                 <Link to="/">
-                    <h1>Comic Database</h1>
+                    <h1> WebComic Database </h1>
                 </Link>
                 <nav>
                     {user && (
